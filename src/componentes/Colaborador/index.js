8f74d12e73b772c33103a152a5b3680618afd6c7
robs-am <roberta.amaro89@gmail.com>
2022-10-20 22:13:02 -0300

    componente basico card

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div>
+      <div>
+        <img src="https://github.com/robs-am.png" alt="" />
+      </div>
+      <div>
+        <h4>Roberta Amaro</h4>
+        <h5>Desenvolvedora</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
