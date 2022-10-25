ad0901754eebbb617ce16cba7cf2dcbb5828236f
robs-am <roberta.amaro89@gmail.com>
2022-10-25 03:57:00 -0300

    rodape

--- src/componentes/Rodape/Rodape.js
+++ src/componentes/Rodape/Rodape.js
@@ -2,10 +2,25 @@ import "./Rodape.css";
 
 const Rodape = () => {
   return (
-  <footer className="footer">
-    <img src="/imagens/fundo.png" alt="" />
-
-  </footer>
+    <footer className="footer">
+      <div className="fundo-wrapper">
+        <img src="/imagens/fundo.png" alt="fundo" />
+      </div>
+      <div className="logo-wrapper">
+        <img src="/imagens/logo.png" alt="organo logo" />
+      </div>
+      <nav className="nav-wrapper">
+        <li>
+          <a href="https://www.facebook.com/">Facebook</a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/">Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/">Instagram</a>
+        </li>
+      </nav>
+    </footer>
   );
 };
 
