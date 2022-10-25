8a5a487d2d96df9331126ee1e72f0f737c885c2b
robs-am <roberta.amaro89@gmail.com>
2022-10-25 03:51:56 -0300

    rodape.css

---
+++ src/componentes/Rodape/Rodape.js
@@ -0,0 +1,12 @@
+import "./Rodape.css";
+
+const Rodape = () => {
+  return (
+  <footer className="footer">
+    <img src="/imagens/fundo.png" alt="" />
+
+  </footer>
+  );
+};
+
+export default Rodape;
